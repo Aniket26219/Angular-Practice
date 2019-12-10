d7f6927ce236a899a493edcbbf0e120e499a2830
ANIKET <aniket98shahane@gmail.com>
2019-12-10 11:53:47 +0530

    practicing the dom and some modification in angular project

--- First-Project/src/app/app.module.ts
+++ First-Project/src/app/app.module.ts
@@ -2,12 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { DemoComponent } from './demo/demo.component';
-import { FirstModule } from './first/first.module';
 
 @NgModule({
-  declarations: [AppComponent,DemoComponent],
-  imports: [BrowserModule,FirstModule],
+  declarations: [AppComponent],
+  imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
 })
---
+++ dom-practice-udemy/main.js
@@ -0,0 +1,3 @@
+console.log(console)
+console.log(document)
+document.getElementById('android').style.border = '1px solid orange'
